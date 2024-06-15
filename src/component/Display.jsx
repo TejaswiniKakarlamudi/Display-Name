54cbe8916533db1460e4447b8776dfde1b2f0e5a
TejaswiniKakarlamudi <kakarlamuditejaswini@gmail.com>
2024-06-15 19:30:26 +0530

    Display name

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -2,46 +2,42 @@ import styles from './Display.module.css';
 import React, { useState } from 'react';
 
 function Display() {
-    const [fname, setfname] = useState('');
+    const [fname, setFname] = useState('');
     const [lname, setLname] = useState('');
     const [funame, setFuname] = useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
         setFuname(`${fname} ${lname}`);
-        setfname('');
-        setLname('');
     }
 
     return (
         <div className={styles.set}>
             <h1 className={styles.heading}>Full Name Display</h1>
-            <div className={styles.formContainer}>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.inputContainer}>
-                        <label>First Name : </label>
-                        <input 
-                            type="text" 
-                            placeholder="First Name" 
-                            value={fname} 
-                            onChange={(e) => setfname(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <label>Last Name : </label>
-                        <input 
-                            type="text" 
-                            placeholder="Last Name" 
-                            value={lname} 
-                            onChange={(e) => setLname(e.target.value)}
-                            required
-                        />
-                    </div>
+            <form onSubmit={handleSubmit} className={styles.formContainer}>
+                <div className={styles.inputContainer}>
+                    <label>First Name:</label>
+                    <input 
+                        type="text" 
+                        placeholder="First Name" 
+                        value={fname} 
+                        onChange={(e) => setFname(e.target.value)}
+                    />
+                </div>
+                <div className={styles.inputContainer}>
+                    <label>Last Name:</label>
+                    <input 
+                        type="text" 
+                        placeholder="Last Name" 
+                        value={lname} 
+                        onChange={(e) => setLname(e.target.value)}
+                    />
+                </div>
+                <div>
                     <button type="submit">Submit</button>
-                </form>
-            </div>
-            {funame && <div className={styles.result}>Full Name: {funame}</div>}
+                </div>
+            </form>
+            {funame && <h3>{funame}</h3>}
         </div>
     );
 }
